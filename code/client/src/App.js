ba723fa69d4b8e5e58becaf7903c374d1feb178a
veereshk20 <pandiriveereshkumar@gmail.com>
2024-04-17 12:09:57 +0530

    added codes folders and necessary starting code folders and files for node and react

---
+++ code/client/src/App.js
@@ -0,0 +1,32 @@
+import React,{useEffect, useState} from 'react'
+
+function App() {
+
+  const [backendData, setBackendData] = useState([{}])
+
+  useEffect(() => {
+    fetch("/api").then(
+      response => response.json()
+    ).then(
+      data => {
+        setBackendData(data)
+      }
+    )
+  },[])
+  //this [] is provided so that this useEffect will work only at the first render of the component
+
+  return (
+    <div>
+      {(typeof backendData.users === 'undefined') ? (
+        <p>Loading...</p>
+      ):(
+        backendData.users.map((user,i)=>(
+          <p key={i}>{(user)}</p>
+        ))
+      )}
+
+    </div>
+  )
+}
+
+export default App
