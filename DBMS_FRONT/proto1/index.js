3750f40611661f94f7f8e759a194f7dcf61ff3ed
Ashrith <141637469+Ashrithiiitdm@users.noreply.github.com>
2024-05-15 22:50:46 +0530

    Updated index.js

--- DBMS_FRONT/proto1/index.js
+++ DBMS_FRONT/proto1/index.js
@@ -2,63 +2,87 @@
 
 
 
-const express = require ('express');
-const passport = require ('passport');
-const session = require ('express-session');
-const path = require ('path');
-const app = express ();
-require ('./auth');
-app.use (express.json ());
-app.use (express.static (path.join (__dirname, 'client')));
+const exp = require('constants');
+const express=require('express');
+const path=require('path')
+const app=express();
+const passport=require('passport')
+const session=require('express-session')
+const mysql=require('mysql')
+require('./auth')
+app.use(express.json());
+//app.use(express.static(path.join(__dirname,'client')))
+// Assuming your CSS files are in a directory named 'public'
+app.use(express.static(path.join(__dirname, '')));
+function isLoggedIn(req,res,next){
+    req.user ? next() : res.sendStatus(401);
+ }
+app.get('/',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'./home.html'))
+})
 
-function isLoggedIn (req, res, next) {
-  req.user ? next () : res.sendStatus (401);
-}
-
-app.get ('/', (req, res) => {
-  res.sendFile ('home.html');
-});
-
-app.use (
-  session ({
+app.use(session({
     secret: 'mysecret',
     resave: false,
     saveUninitialized: true,
-    cookie: {secure: false},
-  })
-);
+    cookie: { secure: false }
+}))
 
-app.use (passport.initialize ());
-app.use (passport.session ());
-app.get (
-  '/auth/google',
-  passport.authenticate ('google', {
-    scope: ['email', 'profile'],
-  })
-);
+app.use(passport.initialize());
+app.use(passport.session());
+app.get('/auth/google',
+  passport.authenticate('google', { scope:
+      [ 'email', 'profile' ] }
+));
 
-app.get (
-  '/auth/google/callback',
-  passport.authenticate ('google', {
-    successRedirect: '/auth/protected',
-    failureRedirect: '/auth/google/failure',
-  })
-);
+app.get( '/auth/google/callback',
+    passport.authenticate( 'google', {
+        successRedirect: '/auth/google/success',
+        failureRedirect: '/auth/google/failure'
+}));
 
-app.get ('/auth/google/failure', (req, res) => {
-  res.send ('Something went wrong!');
+app.get('/auth/google/failure',isLoggedIn,(req,res)=>{
+    res.send('Something wrong')
 });
-
-app.get ('/auth/protected', isLoggedIn, (req, res) => {
-  let name = req.user.displayName;
-  res.send (`Hello ${name}`);
+let Name=""
+let mail=""
+let roll_no=""
+app.get('/auth/google/success',isLoggedIn,(req,res)=>{
+    
+    Name=req.user.name.familyName;
+    //console.log(name)
+    mail=req.user.email
+    roll_no=req.user.given_name
+    queryUser(Name,mail,roll_no)
+    res.redirect('/profile.html');
 });
+async function queryUser(Name,mail,roll_no){
+    const con=mysql.createConnection({
+        host:'localhost',
+        user:'root',
+        password:'password',
+        database:'Sports'
+    })
+    
+    con.connect((err)=>{
+        if(err){
+            console.log("error connect")
+            return;
+        }
+        console.log("Connected")
+        con.query(`select S.timing from slots S join plays P on P.slotid=S.slotid where id='${roll_no}';`,(err,res)=>{
+            if(err){
+                console.log(err)
+            }
+            let timing=res[0].timing
+            return timing 
+        })
+    })
+}
 
-app.use ('/auth/logout', (req, res) => {
-  req.session.destroy ();
-  res.send ('See you again!');
-});
 
-app.listen (5000, () => {
-  console.log ('Listening on port 5000');
+
+
+app.listen(5500,()=>{
+    console.log('Listening on port 5500')
 });
