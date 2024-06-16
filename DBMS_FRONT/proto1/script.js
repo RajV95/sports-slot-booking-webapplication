f1642ed37b3624178ed0b4c610e66865f7bc0b00
Ashrithiiitdm <cs22b2025@iiitdm.ac.in>
2024-06-16 17:54:51 +0530

    merge resolved

--- DBMS_FRONT/proto1/script.js
+++ DBMS_FRONT/proto1/script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 let menu = document.querySelector('#menu-icon');
 let navList = document.querySelector('.navList');
 
@@ -21,27 +20,3 @@ sr.reveal('.scroll-down', {delay: 500, origin: 'bottom'});
 
 
 
-=======
-let menu = document.querySelector('#menu-icon');
-let navList = document.querySelector('.navList');
-
-menu.onclick = () => {
-    menu.classList.toggle('bx-x');
-    navList.classList.toggle('open');
-};
-
-const sr = ScrollReveal ({
-    distance: '65px',
-    duration: 2600,
-    delay: 450,
-    reset: true
-});
-
-sr.reveal('.hero-text', {delay: 200, origin: 'top'});
-sr.reveal('.hero-img', {delay: 450, origin: 'top'});
-sr.reveal('.icons', {delay: 500, origin: 'bottom'});
-sr.reveal('.scroll-down', {delay: 500, origin: 'bottom'});
-
-
-
->>>>>>> 4ec76147f5b6b7c427930e69d20b60582e5430e6
