61eef0e19e2dde122eae4b60df7121d8bf7d6793
Ashrithiiitdm <cs22b2025@iiitdm.ac.in>
2024-06-16 17:51:22 +0530

    Updated scriptprofile.js

--- DBMS_FRONT/proto1/scriptProfile.js
+++ DBMS_FRONT/proto1/scriptProfile.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 let menu = document.querySelector('#menu-icon');
 let navList = document.querySelector('.navList');
 
@@ -24,30 +23,3 @@ function changePhNo(){
         alert("Phone number should contain exactly 10 numeric characters.");
     }
 }
-=======
-let menu = document.querySelector('#menu-icon');
-let navList = document.querySelector('.navList');
-
-menu.onclick = () => {
-    menu.classList.toggle('bx-x');
-    navList.classList.toggle('open');
-};
-
-const sr = ScrollReveal ({
-    distance: '65px',
-    duration: 2600,
-    delay: 450,
-    reset: true
-});
-
-sr.reveal('.icons', {delay: 500, origin: 'bottom'});
-
-function changePhNo(){
-    var newPhoneNumber = prompt("Enter new phone number:");
-    if (/^\d{10}$/.test(newPhoneNumber)) {
-        document.getElementById("phone").innerText = newPhoneNumber;
-    } else {
-        alert("Phone number should contain exactly 10 numeric characters.");
-    }
-}
->>>>>>> 4ec76147f5b6b7c427930e69d20b60582e5430e6
